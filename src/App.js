75014bd3a5cdd83c43cf3f74aa20efa59527ad64
NewbieAnderson <engineeringsong@gmail.com>
2023-01-03 23:46:26 +0900

    change page name "Main" to "Index"

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { Route, Routes } from "react-router-dom";
 import MainLayout from "./components/MainLayout/MainLayout";
 import Add from "./pages/Add";
 import Daily from "./pages/Daily";
-import Main from "./pages/Main";
+import Index from "./pages/Main";
 import Memory from "./pages/Memory";
 
 function App() {
@@ -12,7 +12,7 @@ function App() {
     <div css={AppStyle}>
       <Routes>
         <Route element={<MainLayout />}>
-          <Route path="/" element={<Main />} />
+          <Route path="/" element={<Index />} />
           <Route path="/add" element={<Add />} />
           <Route path="/memory" element={<Memory />} />
           <Route path="/daily" element={<Daily />} />
--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,5 +1,5 @@
 import React from "react";
 
-export default function Main() {
+export default function Index() {
   return <div>Main</div>;
 }
