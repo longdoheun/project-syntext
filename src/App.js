8dad9059f002dc0f8632d758cd0ead36af88488b
longdoheun <george1119@naver.com>
2023-01-03 23:42:24 +0900

    coloring layout

--- src/App.js
+++ src/App.js
@@ -25,6 +25,10 @@ function App() {
 const AppStyle = css`
   width: 100%;
   height: 100%;
+  display: flex;
+  flex-direction: column;
+  /* justify-content: center; */
+  align-items: center;
 `;
 
 export default App;
