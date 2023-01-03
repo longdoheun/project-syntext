7b50b472f118837926c2bee914f1874c7c0fbe42
NewbieAnderson <engineeringsong@gmail.com>
2023-01-04 01:29:48 +0900

    changed features of index pages

--- src/components/Index/Content.jsx
+++ src/components/Index/Content.jsx
@@ -1,5 +1,6 @@
 /** @jsxImportSource @emotion/react */
 import { css } from "@emotion/react";
+import { NotoSans } from "../../styles/GlobalFonts";
 
 export default function Content(props) {
   const { tags, content } = props.metaData;
@@ -12,22 +13,25 @@ export default function Content(props) {
 }
 
 const warpperStyle = css`
-  background-color: #FFFFFF;
+  ${NotoSans}
+  background-color: #F6F6F6;
+  padding: 10px;
+  margin-bottom: 10px;
+  width: 100%;
 `;
 
 const tagStyle = css`
-  font-family: 'Noto Sans KR', sans-serif;
+  display: inline-block;
+  margin: .25em 2px;
+  padding: .2em .5em .3em;
   background-color: #15C39A;
   color: #FFFFFF;
-  font-size: 15px;
-  //margin-right: 5px;
-  display: inline-block;
-  border-radius: 3px;
-  padding: .2em .5em .3em;
+  font-size: 12px;
   font-weight: 600;
-  margin: .25em 3px;
+  border-radius: 3px;
 `;
 
 const contentStyle = css`
-  background-color: #FFFFFF;
+  margin: 0 0.5em 0.3em;
+  font-size: 15px;
 `;
